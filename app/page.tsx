5b21d4026076fad86ae5d4488d3d96918a96b892
Balu <balupasumarthi1@gmail.com>
2026-02-12 09:44:15 +0530

    updated deployed url

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://localhost:3000/dashboard",
+        redirectTo: "https://smart-booking-eight.vercel.app/dashboard",
       },
     });
   };
